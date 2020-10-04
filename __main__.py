55f89b3cfc76ff3ce677e223a16e68bfed653e97
keirbailey <keirinvestment@gmail.com>
2020-10-04 13:53:24 +0100

    Refactored main to absorb data handler change

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 from sklearn.pipeline import Pipeline
 
+from datahandler import DataHandler
 from repository import MongoRepository
 from service import FinancialStatementModellingService
 from transformer import DropColumnsTransformer
@@ -7,17 +8,21 @@ from transformer import DropColumnsTransformer
 
 if __name__ == '__main__':
     # Todo: Construct dependencies somewhere else (factory?) and inject into service
+
     # Repository Integration
     repository = MongoRepository("nonprod")
+
+    # Data Handler
+    datahandler = DataHandler(repository)
 
     # Full Transformation Pipeline
     transform_pipeline = Pipeline([
         ('drop_columns_transformer', DropColumnsTransformer(["link", "finalLink"]))
     ])
-    # Other required objects...
 
     # Create service instance w/ injected dependencies & run
-    service = FinancialStatementModellingService(repository, transform_pipeline)
+    service = FinancialStatementModellingService(datahandler, transform_pipeline)
+
     df = service.run_service()
 
 
